d95e8a7b785246ad686cd9f5ab247e4b541af183
hamed <hamedsher7edu@gmail.com>
2024-11-29 00:44:02 +0300

    changing1 second and third

---
+++ second.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("hamed second\n");
+
+    return 10;
+
+}
---
+++ third.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("hamed branch");
+
+    return 10;
+
+}
